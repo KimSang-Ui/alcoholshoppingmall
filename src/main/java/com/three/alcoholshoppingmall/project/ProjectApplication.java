90bd6a4d9a6a556db6e13828e7ee072cd4108d5d
topingkkukkasoboro <155594382+topingkkukkasoboro@users.noreply.github.com>
2024-04-19 17:44:52 +0900

    20240419

--- src/main/java/com/three/alcoholshoppingmall/project/ProjectApplication.java
+++ src/main/java/com/three/alcoholshoppingmall/project/ProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectApplication {
 
+	//kbs brach 추가
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
