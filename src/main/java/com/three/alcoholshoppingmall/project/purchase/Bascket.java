5fa3f83174188d5a26696b4e2d7327744bd3f957
dron512 <parkmyounghoi@gmail.com>
2024-04-24 16:26:49 +0900

    240424

--- src/main/java/com/three/alcoholshoppingmall/project/purchase/Bascket.java
+++
@@ -1,6 +0,0 @@
-package com.three.alcoholshoppingmall.project.purchase;
-
-public enum Bascket {
-    PickUp,Delivery
-    // 픽업 장바구니, 배달 장바구니
-}
--- src/main/java/com/three/alcoholshoppingmall/project/purchase/PurchaseStatus.java
+++
@@ -1,6 +0,0 @@
-package com.three.alcoholshoppingmall.project.purchase;
-
-public enum PurchaseStatus {
-    PickUp,Delivery,Wishlist
-    // 픽업, 배달, 찜 목록 결제
-}
